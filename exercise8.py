8c1e2b9b3433b23a883bd1f31d6de2b09b1bcf23
apusaha10 <apusaha9380@gmail.com>
2025-07-22 21:04:58 +0600

    Challenge 8 is complete

---
+++ exercise8.py
@@ -0,0 +1,10 @@
+# Coding challege : 8 Ask the user for a number and tell them if it's a 3-digit number (positive only).
+
+num = input("Enter Number: ")
+
+if (len(num) == 3):
+    print("Its a 3 digit Number")
+else:
+    print("It is not a 3 digit number")
+    
+# challenge 8 is completed
