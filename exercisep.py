3c85aae3cf2a0c3c80841ff9f600fc075b2e894e
apusaha10 <apusaha9380@gmail.com>
2025-07-22 21:50:54 +0600

    Challenge 9 is complete

---
+++ exercisep.py
@@ -0,0 +1,14 @@
+# Code challenge 9:  9. Name Formatter
+# Ask for a full name like “john doe”
+# Capitalize both parts (output: “John Doe”)
+# (Use .capitalize() and string slicing logic)
+
+firstName = input("Enter Your First Name: ")
+lastName = input("Enter your last Name: ")
+
+fullName = firstName.capitalize() + " " + lastName.capitalize()
+
+print(fullName)
+
+
+# challenge  10 is completed
