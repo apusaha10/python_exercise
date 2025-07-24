4f442a862648e6c14e112505e977a09f41129609
apusaha10 <apusaha9380@gmail.com>
2025-07-24 19:45:57 +0600

    Challenge is complete

---
+++ exercise17.py
@@ -0,0 +1,10 @@
+# Challenge 17: Access Tuple elment Given the tuple:
+# info = ("Apu", 25, "Bangladesh")
+
+# # Print only the name and country (not age).
+
+
+info = ("Apu", 25, "Bangladesh")
+
+print(info[0])
+print(info[2])
