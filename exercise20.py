61de5de5624aa5ad6f99fcb16f7486985e8015ac
apusaha10 <apusaha9380@gmail.com>
2025-07-24 20:38:17 +0600

    Challenge is complete

---
+++ exercise20.py
@@ -0,0 +1,9 @@
+# challenge 20 : Check if tuple has exactly 3 items
+# Create a tuple and print whether it has exactly 3 items.
+
+numbers = (56, 25, 36,)
+
+if (len(numbers) == 3):
+    print("The tuple has 3 items")
+else:
+    print("This tuple does not have 3 items") 
