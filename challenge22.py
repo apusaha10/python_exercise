697f3cf854e3d28517ee22f05db7d3f52285bd32
apusaha10 <apusaha9380@gmail.com>
2025-07-24 22:08:36 +0600

    Challenge is complete

---
+++ challenge22.py
@@ -0,0 +1,6 @@
+# challenge 22 : Swap two items in a list
+# Create a list of 3 elements and swap the first and last items, then print the list.
+
+colors = ["Red", "Green", "Blue"]
+colors.reverse()
+print(colors)
