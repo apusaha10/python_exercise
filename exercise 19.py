775ed15a9410a0e33c154da5cbdbca0e6d3a745e
apusaha10 <apusaha9380@gmail.com>
2025-07-24 20:33:11 +0600

    Challenge is complete

---
+++ exercise 19.py
@@ -0,0 +1,15 @@
+# challenge 19:  7: Compare lengths of two lists
+# You have two lists. Check which one has more items.
+
+# list1 = [1, 2, 3]
+# list2 = [4, 5]
+# # Output: List 1 is longer
+
+list1 = [1, 2, 3]
+list2 = [4, 5]
+
+
+if (len(list1)> len(list2)):
+    print("List 1 is longer")
+else:
+    print("List 2 is longer")
