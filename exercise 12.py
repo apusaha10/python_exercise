7de9c4cb8a836acc32a853eb7cdfc6348a954d54
apusaha10 <apusaha9380@gmail.com>
2025-07-24 19:18:57 +0600

    Challenge 12 is complete

--- exercise 12.py
+++ exercise 12.py
@@ -1,12 +1,12 @@
 # challenge 12: Task: Ask the user for 5 numbers, store them in a list, and print how many are even and how many are odd.
 
+# Take 5 numbers from user
 numbers = []
-
-num1 = input("Please enter first number")
-num2 = input("Please enter second number")
-num3 = input("Please enter third number")
-num4 = input("Please enter Fourth number")
-num5 = input("Please enter Fifth number")
+num1 = int(input("Please enter first number: "))
+num2 = int(input("Please enter second number: "))
+num3 = int(input("Please enter third number: "))
+num4 = int(input("Please enter fourth number: "))
+num5 = int(input("Please enter fifth number: "))
 
 numbers.append(num1)
 numbers.append(num2)
@@ -14,7 +14,15 @@ numbers.append(num3)
 numbers.append(num4)
 numbers.append(num5)
 
-if (numbers%2 == 0):
-    print("Numbers are even")
-else:
-    print("odd numbers")
+even_count = 0
+odd_count = 0
+
+# Loop through the list to count even and odd numbers
+for number in numbers:
+    if number % 2 == 0:
+        even_count += 1
+    else:
+        odd_count += 1
+
+print("Total even numbers:", even_count)
+print("Total odd numbers:", odd_count)
