6820ee2455fd0f087390fe3fb7e2f951286bb9e4
apusaha10 <apusaha9380@gmail.com>
2025-07-31 19:33:18 +0600

    Challenge is complete

---
+++ challenge26.py
@@ -0,0 +1,20 @@
+# Set related challenges: 
+# challenge 26: 4. Create a Set of Fruits
+# Task:
+# Create a set named fruits with the items: "apple", "banana", "orange".
+# Then:
+
+# Add "grape" to the set.
+
+# Remove "banana" from the set.
+
+# Print the updated set.
+
+items = {"apple", "banana", "orange"}
+
+items.add("grape")
+items.remove("banana")
+print(items)
+
+
+
