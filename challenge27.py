886d52df4843510edb769f5315681e18e019b353
apusaha10 <apusaha9380@gmail.com>
2025-07-31 19:39:25 +0600

    Challenge is complete

---
+++ challenge27.py
@@ -0,0 +1,20 @@
+# challenge 27 : Task:
+# Create a set of colors:
+
+# python
+# Copy
+# Edit
+# colors = {"red", "green", "blue"}
+# Then:
+
+# Check if "yellow" is in the set.
+
+# If it is not, print "Yellow not found".
+
+
+colors = {"red", "green", "blue"}
+
+if ("yellow" in colors):
+    print("Yellow found")
+else:
+    print("Yellow not found")
