ec3fec90c138dcb319f0c4aa94142e310538890b
apusaha10 <apusaha9380@gmail.com>
2025-07-31 19:23:10 +0600

    Challenge is complete

---
+++ challenge25.py
@@ -0,0 +1,12 @@
+# challenge 24: Given the dictionary:
+# person = {"name": "John", "age": 25}
+# Check if the key "email" exists in the dictionary and print "Exists" or "Does not exist" using an if statement.
+
+
+person = {"name": "John", "age": 25}
+
+
+if ("email" in person) :
+    print("exists")
+else:
+    print("Does not exist")
