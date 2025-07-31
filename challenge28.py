0213a7ad17792ca106d6c607bcd01c02e52ef4a3
apusaha10 <apusaha9380@gmail.com>
2025-07-31 19:43:02 +0600

    Challenge is complete

---
+++ challenge28.py
@@ -0,0 +1,20 @@
+# challenge 28: Given two sets:
+
+# set1 = {1, 2, 3}
+# set2 = {3, 4, 5}
+# Find:
+
+# The union of set1 and set2
+
+# The intersection of set1 and set2
+
+# Print both results.
+
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+
+set3 = set1.union(set2)
+set4= set1.intersection(set2)
+
+print(set3)
+print(set4)
